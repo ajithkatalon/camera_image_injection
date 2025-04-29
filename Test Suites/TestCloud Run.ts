869aa1b2cccfc87983e2fa7a70ed0dcdacd459b1
ajithkatalon <ajith.kp@katalon.com>
2025-04-29 18:00:01 +0530

    Test Suite Added

---
+++ Test Suites/TestCloud Run.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestCloud Run</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>7e02bd0d-4e2c-47d3-85eb-1c8d990317b7</testSuiteGuid>
+   <testCaseLink>
+      <guid>abd4b48e-7a5a-468a-bbf8-ac4c10f9bd97</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/First test case</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
